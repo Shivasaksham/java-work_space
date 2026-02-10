3b4557bc2e50b26fad824ee28440af21ad8bfc08
Harsh Vardhan Singh <2k24.cs1i.2412697@gmail.com>
2026-02-10 19:24:26 +0530

    Change printed message from 'hello' to 'hello World'

--- App.java
+++ App.java
@@ -3,7 +3,7 @@ public class App {
     public static void main(String[] args) {
         String p="hello";
         System.err.println(p);
-        System.out.print("hello");
+        System.out.print("hello World");
                                                                                             
         int n=5;
         int n2=6;
