8266e187c8517e7fc6a947dc6b1ee6dfd3426eb3
Saksham Yadav <sakshamyadav260705@gmail.com>
2026-02-10 19:16:59 +0530

    new java code for App

---
+++ App.java
@@ -0,0 +1,23 @@
+
+public class App {
+    public static void main(String[] args) {
+        String p="hello";
+        System.err.println(p);
+        System.out.print("hello");
+                                                                                            
+        int n=5;
+        int n2=6;
+        System.out.println(p+(n+n2));
+        System.err.println("hello");
+        System.err.print("hello");
+
+        boolean np = true;
+        if(np){
+            System.err.println("hello world");
+        }
+        if(!(!(!(!np)))){
+            System.err.println("if yes not yes");
+        }
+
+    }
+}
